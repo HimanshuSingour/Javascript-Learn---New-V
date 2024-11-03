80f80629bc63eb45c68f2c29748a07da611e8e0f
HimanshuSingour <singourhimanshu3@gmail.com>
2024-11-03 12:56:44 +0530

    first commit

---
+++ 01_variable.js
@@ -0,0 +1,23 @@
+
+//constant
+const accountId = 12323 // No Change in Future
+
+// variable
+let accountEmail = "singourhimanshu3@gmail.com" // this will change 
+var accountPassword = "213234Hiah" // this will change 
+accountCity = "Jaipur"
+let accountAddress; // given undefine
+
+// accountPassword = "weqweqw" // No prefer but work
+
+/* 
+
+Prefer not to user var because of issue in block scop and functional scope
+
+*/
+
+// Print one Value Only
+console.log(accountPassword)
+
+// Print Multiple value at onec
+console.table([accountId , accountEmail , accountPassword, accountCity, accountAddress])
---
+++ 02_datatype.js
@@ -0,0 +1,30 @@
+"use strict" // treat all JS Code as newer version
+
+// alert("Hellow") // we are using nodeJs not Browser
+
+// console.log("Himamshu"); console.log("Singour") 
+
+// //or
+
+// console.log("Himamshu")
+// console.log("Singour") 
+
+// Number => 2
+// bigInt
+// String => ""
+// boolean => true
+// null -> standalone value
+// undefined -> value not assigned to the variable
+// symbole => unique
+
+// Object
+
+// Remember:
+
+// console.log(typeof(undefined))
+// undefined type = undefined only
+
+// console.log(typeof(null))
+// null type = Object 
+
+
